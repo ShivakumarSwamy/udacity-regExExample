993cb5e16c71b1e690d471293a622f77d99f3caa
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2021-08-19 07:39:27 +0200

    Initial commit

---
+++ src/com/regexExample/RegExTester.java
@@ -0,0 +1,13 @@
+package com.regexExample;
+
+import java.util.regex.Pattern;
+
+public class RegExTester {
+
+    public static void main(String[] args) {
+        String emailRegex = "^(.+)@(.+).(.+)$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        String email = "jaff@gmail.com";
+        System.out.println(pattern.matcher(email).matches());
+    }
+}
